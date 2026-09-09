0f56a67bec7b8e9f467287bdd8f9b4b543352ae4
Kavin Seralathan <kavin.seran@gmail.com>
2026-09-09 13:00:49 -0400

    Added a new route for hello HTML page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World from Kavin Seralathan. I am a student.'
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
